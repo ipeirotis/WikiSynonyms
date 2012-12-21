09591d8dee0345f63ddb193f238f7dd60060d56a
georgegg <giogeorgiou@gmail.com>
2012-12-21 17:22:28 +0200

    Minor css+layout fixing

--- views/home.php
+++ views/home.php
@@ -1,8 +1,12 @@
-<div class="jumbotron">
-  <h1>Search for Synonyms<br/>via Wikipedia!</h1>
-  <p class="lead">
-    Using Mediawiki's structure for wikipedia pages, you can search for term's or phrase's synonyms. <br/>
-    We also provide an API for third party applications/services to use.
-  </p>
-  <a class="btn btn-large btn-success" href="<?php echo $this->make_route('/search') ?>">Search now</a>
+<div class="row">
+  <div class="span12">
+    <div class="jumbotron">
+      <h1>Search for Synonyms<br/>via Wikipedia!</h1>
+      <p class="lead">
+        Using Mediawiki's structure for wikipedia pages, you can search for term's or phrase's synonyms. <br/>
+        We also provide an API for third party applications/services to use.
+      </p>
+      <a class="btn btn-large btn-success" href="<?php echo $this->make_route('/search') ?>">Search now</a>
+    </div>
+  </div>
 </div>
--- views/search.php
+++ views/search.php
@@ -1,48 +1,55 @@
-<div id="newsletter">
-  <div class="container">
-    <h3 class="title">
-      WikiSynonyms is a <u>reliable platform</u> to search synonyms.
-    </h3>
-    <h4 class="pitch">
-      <span class="muted">Please enter a term or a phrase you want to search synonyms for:</span>
-    </h4>
-    <form id="search-form" action="<?php echo $this->make_route('/search') ?>" method="POST">
-      <input id="term" name="term" type="text" <?php if ($term): ?> value="<?php echo $term ?>"<?php endif; ?>><input class="btn btn-subscribe btn-xlarge" type="submit" value="Search">
-    </form>
+<div class="row">
+  <div class="span12">
+    <div id="newsletter">
+      <div class="container">
+        <h3 class="title">
+          WikiSynonyms is a <u>reliable platform</u> to search synonyms.
+        </h3>
+        <h4 class="pitch">
+          <span class="muted">Please enter a term or a phrase you want to search synonyms for:</span>
+        </h4>
+        <form id="search-form" action="<?php echo $this->make_route('/search') ?>" method="POST">
+          <input id="term" name="term" type="text" <?php if ($term): ?> value="<?php echo $term ?>"<?php endif; ?>><input class="btn btn-subscribe btn-xlarge" type="submit" value="Search">
+        </form>
+      </div>
+    </div>
   </div>
-</div>
+</div>    
 <?php if ($synonyms): ?>
-  <div id="results" class="container">
-    <?php if ($synonyms['http'] != 200): ?>
-      <?php if ($synonyms['http'] == 204): ?>
-        <div class="alert alert-error">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-          <strong>Error!</strong> <?php echo $synonyms['message'] ?>
-        </div>
-      <?php endif; ?>
-      <?php if ($synonyms['http'] == 300): ?>
-        <div class="alert">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-          <strong>Warning!</strong> <?php echo $synonyms['message'] ?>
-        </div>
-      <?php endif; ?>
-    <?php else: ?>
-      <h4>Synonyms:</h4>
-    <?php endif; ?>
-    <hr/>
-    <ol>
-      <?php foreach ($synonyms['terms'] as $synonym) : ?>
-        <li>
-          <?php if ($synonyms['http'] != 200): ?>
-          <a class="search-again" href="#"><i class="icon-search"></i> <?php echo $synonym ?></a>
-          <?php else: ?>
-            <?php echo $synonym ?>
+  <div class="row">
+    <div class="span12">
+      <div id="results">
+        <?php if ($synonyms['http'] != 200): ?>
+          <?php if ($synonyms['http'] == 204): ?>
+            <div class="alert alert-error">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong>Error!</strong> <?php echo $synonyms['message'] ?>
+            </div>
           <?php endif; ?>
-        </li>
-      <?php endforeach; ?>
-    </ol>
-
-  </div>
+          <?php if ($synonyms['http'] == 300): ?>
+            <div class="alert">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong>Warning!</strong> <?php echo $synonyms['message'] ?>
+            </div>
+          <?php endif; ?>
+        <?php else: ?>
+          <h4>Synonyms:</h4>
+        <?php endif; ?>
+        <hr/>
+        <ol>
+          <?php foreach ($synonyms['terms'] as $synonym) : ?>
+            <li>
+              <?php if ($synonyms['http'] != 200): ?>
+                <a class="search-again" href="#"><i class="icon-search"></i> <?php echo $synonym ?></a>
+              <?php else: ?>
+                <?php echo $synonym ?>
+              <?php endif; ?>
+            </li>
+          <?php endforeach; ?>
+        </ol>
+      </div>
+    </div> 
+  </div> 
 <?php endif; ?>
 
 <script type="text/javascript">

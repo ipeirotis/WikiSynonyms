a4ca7f76738401470dc67c82d58efa10e4a9de07
georgegg <giogeorgiou@gmail.com>
2013-01-23 19:26:39 +0200

    Refresh Mediawiki tables Add sql file to truncate tables.

--- scripts/odesk-skills-create-insert-sql.php
+++ scripts/odesk-skills-create-insert-sql.php
@@ -1,6 +1,6 @@
 <?php
-include dirname(dirname(__FILE__)).'/config/config.php';
-include dirname(dirname(__FILE__)).'/lib/cron.php';
+require_once dirname(dirname(__FILE__)).'/config/config.php';
+require_once dirname(dirname(__FILE__)).'/lib/cron.php';
 
 set_time_limit(172800);
 

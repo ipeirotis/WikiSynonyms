0466774ffb4b177621c8319b7a930bb70973aca9
georgegg <giogeorgiou@gmail.com>
2013-01-23 19:03:13 +0200

    Refresh odesk skill cron fix

--- lib/cron.php
+++ lib/cron.php
@@ -2,7 +2,8 @@
 
 class Cron
 {
-
+  public static $_fails = 60;
+  
   static function doConnect()
   {
     mysql_connect(DB_HOST, DB_USER, DB_PASS);
@@ -15,7 +16,24 @@ class Cron
   {
     mysql_close();
   }
-
+  
+  static function fetchOdeskSkill($skill) 
+  {
+    $response = file_get_contents('http://www.odesk.com/api/profiles/v1/metadata/skills/' . urlencode($skill) . '.json');
+    $response_header = $http_response_header;
+    if ($response_header[0] == 'HTTP/1.0 400 Bad Request') {
+      echo $skill . ' Failed' . "\n";
+      return false;
+    }
+    if ($response_header[0] != 'HTTP/1.0 200 OK') {
+      self::$_fails;
+      echo $skill . ' Fail : sleep for ' . self::$_fails . ' seconds' . "\n";
+      sleep(self::$_fails);
+      $response = self::fetchOdeskSkill($skill);
+    }
+    return $response;
+  }
+  
   /**
    * Function refreshOdeskSkills
    *
@@ -31,14 +49,18 @@ class Cron
       $odesk_skills = json_decode(file_get_contents('http://www.odesk.com/api/profiles/v1/metadata/skills.json'));
       $skills = $odesk_skills->skills;
 
-      $query = 'INSERT INTO odesk_skills (skill, pretty_name, external_link, description, wikipedia_page_id) VALUES ' . "\n";
+      $query = 'TRUNCATE TABLE ipeirotis.odesk_skills;' . "\n";
+      
+      $query .= 'INSERT INTO odesk_skills (skill, pretty_name, external_link, description, wikipedia_page_id) VALUES ' . "\n";
       $i = 0;
       $count = $stop ? $stop : count($skills);
       foreach ($skills as $k => $skill) {
-        if (!$force) {
-          sleep(5);
+        $response = self::fetchOdeskSkill($skill);
+        if (!$response) {
+          continue;
         }
-        $skill_data = json_decode(file_get_contents('http://www.odesk.com/api/profiles/v1/metadata/skills/' . $skill . '.json'));
+        echo $skill . ' OK ' . "\n";
+        $skill_data = json_decode($response);
         $query .= sprintf("('%s', '%s', '%s', '%s', %s)", mysql_real_escape_string($skill_data->skill->skill), mysql_real_escape_string($skill_data->skill->skill), mysql_real_escape_string($skill_data->skill->external_link), mysql_real_escape_string($skill_data->skill->description), 0
         );
         $i++;
@@ -49,7 +71,7 @@ class Cron
           $query .= ",\n";
         }
       }
-      $fp = fopen(dirname(dirname(__FILE__)) . '/public/assets/cron_p/odesk-skills.sql', 'w');
+      $fp = fopen(dirname(dirname(__FILE__)) . '/web/assets/cron_p/odesk-skills.sql', 'w');
       fwrite($fp, $query);
       fclose($fp);
     } catch (Exception $e) {
--- scripts/odesk-skills-create-insert-sql.php
+++ scripts/odesk-skills-create-insert-sql.php
@@ -2,5 +2,6 @@
 include dirname(dirname(__FILE__)).'/config/config.php';
 include dirname(dirname(__FILE__)).'/lib/cron.php';
 
+set_time_limit(172800);
 
 Cron::refreshOdeskSkills(true);

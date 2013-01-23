573d5873f79a026f621630c3a181bcc8ac988bc0
georgegg <giogeorgiou@gmail.com>
2013-01-23 19:34:01 +0200

    cleanup

--- scripts/odesk-skills-clean-up.php
+++
@@ -1,6 +0,0 @@
-<?php
-include dirname(dirname(__FILE__)).'/config/config.php';
-include dirname(dirname(__FILE__)).'/lib/cron.php';
-
-
-Cron::cleanUpOdeskSkills();
---
+++ scripts/oskills-clean.php
@@ -0,0 +1,6 @@
+<?php
+//include dirname(dirname(__FILE__)).'/config/config.php';
+//include dirname(dirname(__FILE__)).'/lib/cron.php';
+//
+//
+//Cron::cleanUpOdeskSkills();

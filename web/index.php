0188d98453d09895c0c945e073a9d25944eaa63a
georgegg <giogeorgiou@gmail.com>
2013-01-23 19:34:40 +0200

    cleanup

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
-include dirname(dirname(__FILE__)).'/config/config.php';
-include dirname(dirname(__FILE__)).'/lib/bones.php';
-include dirname(dirname(__FILE__)).'/config/routes.php';
+require_once dirname(dirname(__FILE__)).'/config/config.php';
+require_once dirname(dirname(__FILE__)).'/lib/bones.php';
+require_once dirname(dirname(__FILE__)).'/config/routes.php';
 
 resolve();

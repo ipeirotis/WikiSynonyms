fbd177409609f3de83cd4486234542c601baab59
georgegg <giogeorgiou@gmail.com>
2013-02-22 20:24:36 +0200

    update build sql

--- build/sql/page_relations.sql
+++ build/sql/page_relations.sql
@@ -2,5 +2,4 @@ INSERT IGNORE INTO page_relation
 SELECT s.rd_from as sid, t.page_id as tid, p.page_namespace as snamespace, t.page_namespace as tnamespace, p.page_title as stitle, t.page_title as ttitle, p.page_title as stitle_cs, t.page_title as ttitle_cs 
 FROM redirect s 
 JOIN page p ON (s.rd_from = p.page_id)
-JOIN page t ON (s.rd_namespace = t.page_namespace AND s.rd_title = t.page_title)
-WHERE 1
+JOIN page t ON (s.rd_namespace = t.page_namespace AND s.rd_title = t.page_title);
